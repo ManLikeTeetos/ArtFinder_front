b4a156187a7b9189f253a39d82cb4acaaa859d75
ManLikeTeetos <tolu.mastermindz@gmail.com>
2023-10-08 14:01:25 +0100

    Build

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the source and destination directories
+SOURCE_DIR="./build/"
+DESTINATION_DIR="/home/artfinde/public_html/"
+
+# Create the destination directory if it doesn't exist
+mkdir -p $DESTINATION_DIR
+
+# Copy the build files (excluding source code) to the destination directory
+rsync -av --exclude='src/' --exclude='node_modules/' $SOURCE_DIR $DESTINATION_DIR
